1d7421a171e68f0f5fb8cc19af829ed6c573c200
ilyes-shn <infinityilyes@gmail.com>
2021-01-20 15:16:22 +0100

    last

--- src/components/Shoutouts.jsx
+++ src/components/Shoutouts.jsx
@@ -10,7 +10,7 @@ const Shoutouts = () => {
                 <img src="three.png" alt="three"/>
             </div>
             <h5>🙌 Pam, Sammy & Mathew</h5>
-            <a style={{fontSize: '14px'}} href="#">🙌 Pam, Sammy & Mathew</a>
+            <a style={{fontSize: '14px'}} href="/">🙌 Pam, Sammy & Mathew</a>
             <p>Brilliant work with the Hooli account this week, everyone really stepped up and communcated with them well. Controlling expectations is a big win. I was told they’re really happy with the results.</p>
         </div>
     )
